d595337838bdcb91a45b4a16fbbe8ba4086e3f24
Harshita Negi <harshitanegi229@gmail.com>
2023-07-21 12:55:30 +0530

    final blog website with database

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,14 +18,22 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts=[];
 
-app.get("/" , function(req,res){           // CHALLENGE 1
-  
-res.render("home" , {                      // CHALLENGE 2
-   homeContent : homeStartingContent  , 
-   posts : posts});                        // CHALLENGE 12
-// console.log(posts);
+const postSchema = {
+  title : String,
+  body : String
+}
+
+const Post = mongoose.model("post", postSchema);
+
+app.get("/" , function(req,res){               // CHALLENGE 1
+   
+  main()
+  async function main(){
+    res.render("home" , {                      // CHALLENGE 2
+    homeContent : homeStartingContent  , 
+    posts : await Post.find({})});             // CHALLENGE 12
+ }
 })
 
 
@@ -49,29 +59,39 @@ app.post("/compose" , function(req, res){
   
   // console.log(req.body.postTitle);      // CHALLENGE 8
 
-  const post ={                            // CHALENGE 11
-     title : req.body.postTitle,
-     body : req.body.postBody
-  }
+  // const post ={                            // CHALENGE 11
+  //    title : req.body.postTitle,
+  //    body : req.body.postBody
+  // }
 
-  posts.push(post);                       // CHALLENGE 11
-  res.redirect("/");
+  main();
+  async function main(){
+      const post = new Post({
+        title : req.body.postTitle,
+        body : req.body.postBody
+      })
+        await post.save();
+        res.redirect("/");  
+  } 
+
+  // posts.push(post);                       // CHALLENGE 11
+  
 
 })
 
-app.get('/posts/:postName' , function(req , res){
-  const requestedTitle=_.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){          // CHALLENGE 17
-    const storedTitle =_.lowerCase(post.title);   // CHALLENGE 18
-    if(requestedTitle===storedTitle){
-      res.render("post", { title : post.title, content : post.body } )  // CHALLENGE 19
-    }
-  
-  })
-
-
- // console.log(req.params.postName);       // CHALLENGE 16
+app.get('/posts/:postId' , function(req , res){
+  const requestedId=(req.params.postId);
+  main()
+  async function main(){
+     try{
+     const posts = await Post.findOne({_id : requestedId})
+       res.render("post", {title : posts.title, content:posts.body})  
+    
+     }catch(e){
+      console.log(e.message);
+     }
+     
+  }
 })
 
 app.listen(3000, function() {
